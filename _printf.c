936db4594a2fd755cd190b5e0f9afbe80ced5e04
Muriuki Muchiri <muriuki@muchir.com>
2023-07-22 20:22:04 +0000

    Hotfix

--- _printf.c
+++ _printf.c
@@ -26,7 +26,6 @@ int _printf(const char *format, ...)
 		{
 			if (_check_specifier(format[i + 1], &count, args) == -1)
 				return (-1);
-			_check_specifier(format[i + 1], &count, args);
 			i++;
 		}
 
