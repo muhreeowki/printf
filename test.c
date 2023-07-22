6e155dc5b84c4d7cb08e47c97ede526dea562a97
Muriuki Muchiri <muriuki@muchir.com>
2023-07-22 15:52:02 +0000

    Fixed _puthex helper function

--- test.c
+++ test.c
@@ -4,6 +4,10 @@
 int main (void)
 {
 	unsigned int n = 255;
-	printf("Count: %i\n", _printf("Number %X\n", n));
+
+	printf("Normal Printf:\n");
+	printf("Count: %i\n\n", printf("Number %X\n", n));
+	printf("Our Printf:\n");
+	printf("Count: %i\n\n", _printf("Number %X\n", n));
 	return (0);
 }
