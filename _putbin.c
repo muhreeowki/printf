8df1aa9d7ac2b5806f959272ad3b522c63fb3371
kevosa <yegokelvin00@gmail.com>
2023-07-22 14:14:14 +0000

    betty doc for putbin

--- _putbin.c
+++ _putbin.c
@@ -1,5 +1,14 @@
 #include "main.h"
-
+/**
+ * _putbin - Print binary representation of an unsigned integer.
+ *
+ * Recursively converts and prints the binary digits of 'n'.
+ *
+ * @n: Unsigned integer to convert and print in binary form.
+ * @count: Pointer to the count of characters printed so far.
+ *
+ * Return: None (void).
+ */
 void _putbin(unsigned int n, int *count)
 {
 	if (n > 1)
