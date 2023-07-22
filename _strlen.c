5050ced5b14a50e8a929c41cd0f8d7bb973f8c5a
kevosa <yegokelvin00@gmail.com>
2023-07-22 11:31:53 +0000

    changes to strlen and addition of function for integers

--- _strlen.c
+++ _strlen.c
@@ -1,11 +1,17 @@
 #include "main.h"
-
+/**
+ * _strlen - Calculate the length of a null-terminated string.
+ *
+ * @str: The input string.
+ *
+ * Return: The length of the string.
+ */
 
 int _strlen(char *str)
 {
-	int length=0;
+	int length = 0;
 
-	while(str[length] != '\0')
+	while (str[length] != '\0')
 	{
 		length++;
 	}
