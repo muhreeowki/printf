7941e5432ea02c42bd377856abee4c3256badcae
kevosa <yegokelvin00@gmail.com>
2023-07-23 14:36:33 +0000

    function for printable strings

---
+++ _print_sstring.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _print_string - Print a string to stdout.
+ *
+ * @args: Variable arguments list containing additional arguments.
+ * @count: Pointer to the count of characters printed so far.
+ *
+ * Return: None (void).
+ */
+void _print_string(va_list args, int *count)
+{
+	int i;
+	char *str;
+
+	str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+
+
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if(str[i] < 32 || str[i] >=127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			(*count) += 2;
+			_print_hex((unsigned int)str[i], count);
+		}
+		else
+		{
+			_putchar(str[i]);
+			(*count)++;
+		}
+	}
+}
